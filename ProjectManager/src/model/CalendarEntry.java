a3812524289fdfd4a4be5dc4b22b77dc1902522e
Viktor Tanyi <tanyi.viktor@gmail.com>
2013-11-16 20:28:14 +0100

    Add calendar entries

---
+++ ProjectManager/src/model/CalendarEntry.java
@@ -0,0 +1,30 @@
+package model;
+
+import java.util.Date;
+
+public class CalendarEntry {
+	// TODO add an optional reference to a Project if the entry belongs to one
+	
+	private String name;
+	private String description;
+	private Date date;	
+	
+	public CalendarEntry(String name, String description, Date date) {
+		this.name = name;
+		this.description = description;
+		this.date = date;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+
+}
--- ProjectManager/src/model/Model.java
+++ ProjectManager/src/model/Model.java
@@ -1,6 +1,22 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Observable;
 
 public class Model extends Observable {
+	private ArrayList<CalendarEntry> entries = new ArrayList<CalendarEntry>();
+	
+	public void addEntry(CalendarEntry e) {
+		entries.add(e);
+	}
+	
+	public void removeEntry(CalendarEntry e) {
+		entries.remove(e);
+	}
+	
+	public void modifyEntry(CalendarEntry from, CalendarEntry to) {
+		if (entries.remove(from)) {
+			entries.add(to);
+		}
+	}
 }
