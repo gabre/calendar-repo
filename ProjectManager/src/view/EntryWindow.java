5a87c6bd16eddb7ba6e32c4fc0dee6b300f6d6c9
Viktor Tanyi <tanyi.viktor@gmail.com>
2013-11-17 19:32:23 +0100

    Improve calendar entry window (nice layouts)

--- ProjectManager/src/view/EntryWindow.java
+++ ProjectManager/src/view/EntryWindow.java
@@ -1,16 +1,27 @@
 package view;
 
+import java.text.SimpleDateFormat;
+
+import model.CalendarEntry;
 import app.ProjectManagerApplication;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 
 public class EntryWindow extends Window {
 	private ProjectManagerApplication app;
+	private CalendarEntry currentEntry;
+	
+	private Text dateText = new Text();
+	private TextField nameField = new TextField();
+	private TextField descriptionField = new TextField();
 
 	public EntryWindow(ProjectManagerApplication app) {
 		this.app = app;
@@ -23,25 +34,62 @@ public class EntryWindow extends Window {
 
 	@Override
 	public Parent getView() {
-		final AnchorPane mainPane = new AnchorPane();
+		BorderPane mainPane = new BorderPane();
+		mainPane.setPadding(new Insets(10));
 		
-		final GridPane gridPane = new GridPane();
-		gridPane.add(new Text("Dátum:"), 0, 0);
-		gridPane.add(new Text("ggg gg"), 1, 0);
+		GridPane gridPane = new GridPane();
+		gridPane.setPadding(new Insets(10));
+		gridPane.setHgap(15);
+		gridPane.setVgap(5);
 		
-		Button exitButton = new Button("OK");
-		exitButton.setOnAction(new EventHandler<ActionEvent>() {
+		gridPane.addRow(0, new Text("Dátum:"),  dateText);
+		gridPane.addRow(1, new Text("Név:"),    nameField);
+		gridPane.addRow(2, new Text("Leírás:"), descriptionField);
+		
+		HBox buttons = new HBox();
+		buttons.setSpacing(5);
+		
+		Button submitButton = new Button("OK");
+		submitButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
 				app.entryWindowClosed();
 			}
 		});
 		
-		mainPane.getChildren().addAll(gridPane, exitButton);
-		AnchorPane.setTopAnchor(gridPane, 10.);
-		AnchorPane.setBottomAnchor(exitButton, 10.);
+		Button resetButton = new Button("Visszaállít");
+		resetButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				updateFields();
+			}
+		});
+		
+		Button cancelButton = new Button("Mégse");
+		cancelButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				app.entryWindowClosed();
+			}
+		});
+		
+		buttons.getChildren().addAll(submitButton, resetButton, cancelButton);
+		
+		mainPane.setCenter(gridPane);
+		mainPane.setBottom(buttons);
 		
 		return mainPane;
+	}
+	
+	public void setCurrentEntry(CalendarEntry entry) {
+		currentEntry = entry;
+		updateFields();
+	}
+	
+	private void updateFields() {
+		dateText.setText(new SimpleDateFormat("yyyy. MM. dd.").format(currentEntry.getDate()));
+		nameField.setText(currentEntry.getName());
+		descriptionField.setText(currentEntry.getDescription());
 	}
 
 }
