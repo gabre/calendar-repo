2030970283d1cf0fed438c7191915157eaffe5da
Viktor Tanyi <tanyi.viktor@gmail.com>
2013-11-17 19:31:49 +0100

    Improve window creation

--- ProjectManager/src/app/ProjectManagerApplication.java
+++ ProjectManager/src/app/ProjectManagerApplication.java
@@ -1,16 +1,16 @@
 package app;
  
+import java.util.Date;
+
 import view.EntryWindow;
 import view.MainWindow;
 import view.Window;
+import model.CalendarEntry;
 import model.Model;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
-import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
  
@@ -21,22 +21,23 @@ public class ProjectManagerApplication extends Application {
 	
 	// View
 	private MainWindow mainWin;
+	private EntryWindow entryWin;
 	private Stage entryWinStage;
 	
 	
 	// ENTRY POINT
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws Exception
-    {
+    public void start(Stage stage) throws Exception {
         model 	= new Model();
         mainWin = new MainWindow();
+        entryWin = new EntryWindow(this);
+        
         createWin(stage, mainWin, 800, 600);
-        entryWinStage = createWin(new EntryWindow(this), 320, 240);
+        entryWinStage = createWin(entryWin, 320, 240);
         
         stage.show();
         stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
@@ -47,6 +48,7 @@ public class ProjectManagerApplication extends Application {
         });
         
         entryWinStage.show();
+        entryWin.setCurrentEntry(new CalendarEntry("szia", "hohoho", new Date()));
     }
     
     public void entryWindowClosed() {
@@ -55,16 +57,14 @@ public class ProjectManagerApplication extends Application {
 
     // This method is used to "create windows" or something like that...
     // The first stage is created "by the platform" (according to the docs)
-    private Stage createWin(Window win, double width, double height) 
-    {
+    private Stage createWin(Window win, double width, double height) {
     	Stage stage = new Stage();
     	createWin(stage, win, width, height);
     	return stage;
     }
     
-    private Stage createWin(Stage stage, Window win, double width, double height) 
-    {
-    	Scene scene = new Scene(win.getView(), width, height);
+    private Stage createWin(Stage stage, Window win, double width, double height) {
+    	Scene scene = new Scene(win.getView());
         // Stylesheet could be added this way:
     	scene.getStylesheets().add("styles.css");
         stage.setScene(scene);
