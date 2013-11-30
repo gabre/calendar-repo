0f0cc95c0e6a949af34112603ebd58b74d731db0
Viktor Tanyi <tanyi.viktor@gmail.com>
2013-11-30 16:43:40 +0100

    Add buttons to list calendar view

--- ProjectManager/src/view/ListCalendarView.java
+++ ProjectManager/src/view/ListCalendarView.java
@@ -4,24 +4,23 @@ import java.util.Date;
 
 import app.ProjectManagerApplication;
 import model.CalendarEntry;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.util.Callback;
 
-public class ListCalendarView extends HBox {
-	
-	private ProjectManagerApplication app;
-	private ListView<CalendarEntry> listView;
+public class ListCalendarView extends VBox {
 
-	public ListCalendarView(ProjectManagerApplication app) {
+	public ListCalendarView(final ProjectManagerApplication app) {
 		super(5);
-		this.app = app;
 		
-		listView = new ListView<>(app.getModel().getCalendarEntries());
+		final ListView<CalendarEntry> listView = new ListView<>(app.getModel().getCalendarEntries());
 		listView.setCellFactory(new Callback<ListView<CalendarEntry>, ListCell<CalendarEntry>>() {
 			@Override
 			public ListCell<CalendarEntry> call(ListView<CalendarEntry> view) {
@@ -31,29 +30,40 @@ public class ListCalendarView extends HBox {
 		listView.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent event) {
-				entryListMouseHandler(event);
+				if (event.getButton().equals(MouseButton.PRIMARY) &&
+						event.getClickCount() == 2 &&
+						!listView.getSelectionModel().isEmpty()) {
+					CalendarEntry sel = listView.getSelectionModel().getSelectedItem();
+					app.calendarEntryOpened(sel);
+				}
 			}
 		});
 		
-		getChildren().addAll(listView);
-	}
-	
-	private void entryListMouseHandler(MouseEvent event) {
-		if (event.getButton().equals(MouseButton.PRIMARY) &&
-				event.getClickCount() == 2 &&
-				!listView.getSelectionModel().isEmpty()) {
-			CalendarEntry sel = listView.getSelectionModel().getSelectedItem();
-			app.calendarEntryOpened(sel);
-		} else if (event.getButton().equals(MouseButton.SECONDARY) &&
-				event.getClickCount() == 2 &&
-				!listView.getSelectionModel().isEmpty()) {
-			CalendarEntry sel = listView.getSelectionModel().getSelectedItem();
-			app.getModel().removeEntry(sel);
-		} else if (event.getButton().equals(MouseButton.MIDDLE)) {
-			CalendarEntry newEntry = new CalendarEntry("Új esemény", "", new Date());
-			app.getModel().addEntry(newEntry);
-			app.calendarEntryOpened(newEntry);
-		}
+		HBox buttons = new HBox(5);
+		Button btnCreate = new Button("Új esemény");
+		Button btnDelete = new Button("Törlés");
+		buttons.getChildren().addAll(btnCreate, btnDelete);
+		
+		btnCreate.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				CalendarEntry newEntry = new CalendarEntry("Új esemény", "", new Date());
+				app.getModel().addEntry(newEntry);
+				app.calendarEntryOpened(newEntry);
+			}
+		});
+		
+		btnDelete.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				if (!listView.getSelectionModel().isEmpty()) {
+					CalendarEntry sel = listView.getSelectionModel().getSelectedItem();
+					app.getModel().removeEntry(sel);
+				}
+			}
+		});
+		
+		getChildren().addAll(listView, buttons);
 	}
 
 }
